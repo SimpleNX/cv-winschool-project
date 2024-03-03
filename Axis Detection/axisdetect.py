4b2f9395ee6108e94c00bab22cd7ef68ff7f7da7
Simple Panigrahi <simplepanigrahi17@gmail.com>
2024-03-04 00:26:29 +0530

    added Hough Tranformations

--- Axis Detection/axisdetect.py
+++ Axis Detection/axisdetect.py
@@ -35,13 +35,19 @@ def Process(vidpath):
 
         #Detecting the straight lines in the image.
         #And drawing them on the original frame
-        lines = cv.HoughLinesP(frame, 1, np.pi/180, 220, minLineLength=100, maxLineGap=70)
 
-        if lines is not None:
-            for i in range(0 , len(lines)):
-                line = lines[i][0]
-                cv.line(frameorg, (line[0], line[1]), (line[2], line[3]), (255,0,0), 2, cv.LINE_AA)
+        threshold = 150
 
+        #For detection of the lines in the edge frame.
+        lines = hough_detection(frame, threshold, -np.pi/2, np.pi/2, frameorg)
+
+        #For drawing the lines on the original image.
+        for rho, theta in lines:
+            x0 = changespace(rho, theta)
+            direction = np.array([x0[1], -x0[0]])
+            pt1 = np.round(x0 + 1000 * direction).astype(int)
+            pt2 = np.round(x0 - 1000 * direction).astype(int)
+            cv.line(frameorg, pt1, pt2, [0, 255, 255], 3, cv.LINE_AA)
 
         cv.imshow("Edged", frameorg)
         k = cv.waitKey(1) & 0xFFF
@@ -50,6 +56,49 @@ def Process(vidpath):
 
     cv.destroyAllWindows()
 
+
+def hough_detection(edges: np.ndarray, threshold:float, min_theta:float, max_theta:float, image) -> np.ndarray:
+    diagonal = math.sqrt(image.shape[0]**2 + image.shape[1]**2)
+
+    theta = 0.2
+    rho = 10
+
+    #Defining the parameters and their range for the calculation
+    thetas = np.arange(min_theta, max_theta, theta)
+    rhos = np.arange(-diagonal, diagonal, rho)
+
+    #Dividing the space into bunch of small cells.
+    num1 = len(thetas)
+    num2 = len(rhos)
+    cells = np.zeros([num1, num2])
+
+    #Calculation values of sines and cosines.
+    sines = np.sin(thetas)
+    cosines = np.cos(thetas)
+
+    #Considering only the edges
+    xe, ye = np.where(edges > 0)
+
+    #Now iterating through the points and plotting them in the Hough Space with parameters m and b.
+    #Also voting for the cells if they have a line passing through them
+    for x,y in zip(xe, ye):
+        for t in range (len(thetas)):
+        #Calculating the actual line in the hough space as rho = x*sin(theta) + y*cos(theta)
+            rho_space = x*sines[t] + y*cosines[t]
+        #Now finding the cell rho nearest to the rho calculating and voting for the cell where it is present.
+            pos_cell = np.where(rho_space > rhos)[0][-1]
+            cells[pos_cell, t] += 1
+
+    #Taking the cell and the coordinates which have been voted the most.
+    matched_rho_idx, matched_theta_idx = np.where(cells>threshold)
+    matched_rho = rhos[matched_rho_idx]
+    matched_theta = thetas[matched_theta_idx]
+
+    polar_coordinates = np.vstack([matched_rho, matched_theta]).T
+    return polar_coordinates
+
+def changespace(radius: np.ndarray, angle: np.ndarray, cv2_setup: bool = True) -> np.ndarray:
+    return radius * np.array([np.sin(angle), np.cos(angle)])
 
 
 
