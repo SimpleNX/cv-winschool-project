600815ca3c2dd6da4d8de6f336f48f3b8e54a8e8
Simple Panigrahi <simplepanigrahi17@gmail.com>
2024-03-10 13:19:55 +0530

    No changes

--- Global_Planner/planner.py
+++ Global_Planner/planner.py
@@ -6,7 +6,8 @@ import math
 
 
 class Node:
-    def __init__(self, x_cd, y_cd):
+
+    def __init__(self, x_cd: int, y_cd: int):
         self.x_cd = x_cd
         self.y_cd = y_cd
         self.child = []
@@ -14,10 +15,11 @@ class Node:
 
 
 class RRT:
-    def __init__(self, start, goal, img_arr, in_time,  step):
+
+    def __init__(self, start, goal, img_arr, in_time: int,  step: int):
         self.tree = Node(start[0], start[1])
         self.goal = Node(goal[0], goal[1])
-        self.nearestNode = Node(0, 0)
+        self.nearestNode = None
         self.time = min(in_time, 150)
         self.img_arr = img_arr
         self.step = step
@@ -37,14 +39,6 @@ class RRT:
             self.nearestNode.child.append(newnode)
             newnode.par = self.nearestNode
 
-    #function to find the distance between two points.
-    #complete
-    def euc_dist(self, node1, s_point):
-        x_diff = node1.x_cd - s_point[0]
-        y_diff = node1.y_cd - s_point[1]
-        dist = np.sqrt(x_diff**2 + y_diff**2)
-        return dist
-
     #Function to randomly sample points in space
     #Complete
     def sample(self):
@@ -52,6 +46,17 @@ class RRT:
         y = random.randint(1, image_arr.shape[0])
         point = np.array([x, y])
         return point
+
+    #Function to extend the point from the sampled point to the nearest node.
+    #Complete
+    def extend(self, l_start, lend):
+        move = self.step*self.unitVec(l_start, lend)
+        new = np.array([l_start.x_cd+move[0], l_start.y_cd+move[1]])
+        if new[0] >= self.img_arr.shape[1]:
+            new[0] = self.img_arr.shape[1]-1
+        if new[1] >= self.img_arr.shape[0]:
+            new[1] = self.img_arr.shape[0]-1
+        return new
 
     #function to check if the there is a obstacle in between the prev node and the sampled point
     #Complete
@@ -78,34 +83,30 @@ class RRT:
     #Function to find nearest node from the sampled point
     #Complete
     def nearest_node(self, root, s_point):
-        if root is None:
+        if not root:
             return
         dist = self.euc_dist(root, s_point)
-        if dist <= self.step:
-            self.nearestNode.x_cd = root.x_cd
-            self.nearestNode.y_cd = root.y_cd
+        if dist <= self.nearDist:
+            self.nearestNode = root
             self.nearDist = dist
 
         for child in root.child:
             self.nearest_node(child, s_point)
 
-    #Function to extend the point from the sampled point to the nearest node.
-    #Complete
-    def extend(self, l_start, lend):
-        move = self.step*self.unitVec(l_start, lend)
-        new = np.array([l_start.x_cd+move[0], l_start.y_cd+move[1]])
-        if new[0] >= self.img_arr.shape[1]:
-            new[0] = self.img_arr.shape[1]-1
-        if new[1] >= self.img_arr.shape[0]:
-            new[1] = self.img_arr.shape[0]-1
-        return new
+    #function to find the distance between two points.
+    #complete
+    def euc_dist(self, node1, s_point):
+        x_diff = node1.x_cd - s_point[0]
+        y_diff = node1.y_cd - s_point[1]
+        dist = np.sqrt(x_diff**2 + y_diff**2)
+        return dist
 
     #Function to check if the goal has been reached or not.
     #complete
     def isgoal(self, s_point):
         step = self.step
         dis = self.euc_dist(self.goal, s_point)
-        if 0 <= dis <= step:
+        if dis <= step:
             return True
         else:
             return False
@@ -113,7 +114,7 @@ class RRT:
     #Function to update the parameters.
     #Complete
     def changePara(self):
-        self.nearestNode = Node(0, 0)
+        self.nearestNode = None
         self.nearDist = 10000
 
     #function to retrace the path from the goal to start
@@ -121,13 +122,11 @@ class RRT:
         #End Condition
         if goal.x_cd == self.tree.x_cd:
             return
-
-        current_point = np.array([goal.x_cd, goal.y_cd])
         self.noWayP += 1
+        current_point = np.array([goal.x_cd, goal.y_cd])
         self.wayPs.insert(0, current_point)
         self.total_dist += self.step
         self.retrace(goal.par)
-
 
 
 #Reading the image and converting it into grayscale.
@@ -174,7 +173,7 @@ rrt_obj.wayPs.insert(0, srt)
 
 #Drawing the iterations
 for i in range(len(rrt_obj.wayPs)-1):
-    cv.line(image_arr, (rrt_obj.wayPs[i][0], rrt_obj.wayPs[i][1]), (rrt_obj.wayPs[i+1][0], rrt_obj.wayPs[i+1][1]), [255,0,0], 1)
+    cv.line(image_arr, (rrt_obj.wayPs[i][0], rrt_obj.wayPs[i][1]), (rrt_obj.wayPs[i+1][0], rrt_obj.wayPs[i+1][1]), [255, 0, 0], 1)
     cv.imshow("The Map", image_arr)
     k = cv.waitKey(1) & 0xFFF
     if k == 27:
@@ -187,6 +186,4 @@ fig = plt.figure("Check")
 plt.imshow(image_arr, cmap='binary')
 plt.show()
 
-
-
-#node parent and node types check once
+#Handle the none errors
